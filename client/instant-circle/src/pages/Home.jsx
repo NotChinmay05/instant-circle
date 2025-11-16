db70cd45b7e5c7d401b60de630e0081ecee28da2
Mayank Singh <vasushmartya@gmail.com>
2025-11-17 02:04:42 +0530

    small changes

--- client/instant-circle/src/pages/Home.jsx
+++ client/instant-circle/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import timerIconAsset from "../assets/timer.png";
-
+import chatIconAsset from "../assets/chat.png";
+import encIconAsset from "../assets/encryption.png";
 
 const HomePage = ({onViewChange}) => {
 
@@ -8,16 +9,23 @@ const HomePage = ({onViewChange}) => {
         <img src={timerIconAsset} alt="Timer Icon" style={{ height: '50px' }}/>
     );
 
+    const chatIcon = (
+        <img src={chatIconAsset} alt="Chat Icon" style={{ height: '50px' }}/>
+    );
+
+    const encIcon = (
+        <img src={encIconAsset} alt="Chat Icon" style={{ height: '50px' }}/>
+    );
+
     const FeatureCard = ({ title, description, icon, alignment = 'left' }) => (
         <div className="feature-card">
             <div className={`card-content ${alignment === 'left' ? 'left-aligned' : 'right-aligned'}`}>
                 {/* Show icon first for left-aligned, last for right-aligned (desktop) */}
-                {alignment === 'left' && icon}
-                <div className="card-text-container" style={alignment === 'right' ? { textAlign: 'right' } : {}}>
+                {icon}
+                <div className="card-text-container">
                     <h2 className="card-title">{title}</h2>
                     <p className="card-description">{description}</p>
                 </div>
-                {alignment === 'right' && icon}
             </div>
         </div>
   );
@@ -28,7 +36,7 @@ const HomePage = ({onViewChange}) => {
         <FeatureCard
           title="Meet Your Mood, Instantly"
           description="Instant chat group formed with people feeling the same as you."
-          icon={timerIcon}
+          icon={chatIcon}
           alignment="right"
         />
 
@@ -44,8 +52,8 @@ const HomePage = ({onViewChange}) => {
         <FeatureCard
           title="What's said in circle, Stays in circle"
           description="We ensure anonymous identity and encryption"
-          icon={timerIcon}
-          alignment="left"
+          icon={encIcon}
+          alignment="right"
         />
     </div>
   );
